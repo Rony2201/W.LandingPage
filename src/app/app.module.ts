23a1f5d8c03fb38f343632efde18a382dd3c189d
Rony <roniely_pinheiro@hotmail.com>
2024-10-26 22:22:34 -0300

    feat: update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavPageComponent } from './components/nav-page/nav-page.component';
+import { NavSideComponent } from './components/nav-side/nav-side.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    NavPageComponent,
+    NavSideComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nav-side/nav-side.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-side',
+  templateUrl: './nav-side.component.html',
+  styleUrls: ['./nav-side.component.css']
+})
+export class NavSideComponent {
+
+}
